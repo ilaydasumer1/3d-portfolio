c551f023c9bc5c60b273108065440d54ec625f58
ilaydasumer1 <ilayda_sumer21@outlook.com>
2023-12-05 16:43:49 +0300

    Third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,25 +10,45 @@ POPUP
   </div>  */}
 function Home() {
   const [ isRotating, setIsRotating ] = useState(false);
+
 const adjustIslandForScreenSize = () => {
   let screenScale = null;
   let screenPosition = [0, -6.5, -43];
   let rotation = [0.1, 4.7, 0];
 
   if(window.innerWidth <758) {
-    screenScale = [0.9, 0.9, 0.9];
+    screenScale = [1.5, 1.5, 1.5];
+    screenPosition=[0, -1.5, 0]
   } else {
-    screenScale = [1, 1, 1];
+    screenScale = [3, 3, 3];
+    screenPosition=[0, -4, -4]
   }
   return [screenScale, screenPosition, rotation]
 }
 
-const [islandScale, islandPosition, islandRotation ] = adjustIslandForScreenSize();
+const adjustPlaneForScreenSize = () => {
+  let screenScale, screenPosition;
+
+  if (window.innerWidth < 768) {
+    screenScale = [1.5, 1.5, 1.5];
+    screenPosition = [0, -1.5, 0];
+  } else {
+    screenScale = [3, 3, 3];
+    screenPosition = [0, -4, -4];
+  }
+  return [screenScale, screenPosition];
+};
+
+const [islandScale, islandPosition, islandRotation ] = 
+adjustIslandForScreenSize();
+const[ planeScale, planePosition ] =
+ adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className={`w-full h-screen bg-transparent ${ isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+        className={`w-full h-screen bg-transparent ${ isRotating ?
+        'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -49,7 +69,12 @@ const [islandScale, islandPosition, islandRotation ] = adjustIslandForScreenSize
             isRotating={isRotating}
             setIsRotating={setIsRotating}
           />
-          <Plane />
+          <Plane
+          isRotating={isRotating}
+          planeScale={planeScale}
+          planePosition={planePosition}
+          rotation={[0, 20, 0]}
+           />
         </Suspense>
       </Canvas>
     </section>
